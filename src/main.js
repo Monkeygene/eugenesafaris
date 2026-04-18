000852e346145a8aa1e4dbcf19b6eaf8dbb8f5ac
Nyoni Safaris <monkeygenesafaris@gmail.com>
2026-04-18 18:37:55 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
 import './styles.css';
 import './nav.js';
 import './tabs.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const video = document.getElementById('heroVideo');
-  if (video) {
-    video.play().catch(() => {});
-  }
-});
