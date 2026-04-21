34a07756805c0683d5f4c3d651201f7ccb6a1f20
Nyoni Safaris <monkeygenesafaris@gmail.com>
2026-04-21 11:28:33 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
 import './styles.css';
 import './nav.js';
 import './tabs.js';
+
+// Create dataLayer FIRST
+window.dataLayer = window.dataLayer || [];
+window.gtag = function(){ window.dataLayer.push(arguments); };
+
+// Load gtag script
+const script = document.createElement("script");
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-F2CBZHQXX1";
+
+// Optional but cleaner: wait for script before config
+script.onload = () => {
+  window.gtag('js', new Date());
+  window.gtag('config', 'G-F2CBZHQXX1', {
+    page_path: window.location.pathname,
+  });
+};
+
+document.head.appendChild(script);
