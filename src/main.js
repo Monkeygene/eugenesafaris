297f03ba3b6165dcd97ede53a0c312173c74982b
Eugene Safaris <monkeygenesafaris@gmail.com>
2026-06-21 13:39:54 +0100

    reset

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import './nav.js';
 import './tabs.js';
+import './weather.js';
 
 // Create dataLayer FIRST
 window.dataLayer = window.dataLayer || [];
--- vite.config.js
+++ vite.config.js
@@ -10,53 +10,14 @@ export default defineConfig({
 
         // Core
         main: path.resolve(__dirname, 'index.html'),
+        krugernationalpark: path.resolve(__dirname, 'kruger-national-park/index.html'),
+        allinclusivesafaris: path.resolve(__dirname, 'kruger-national-park/all-inclusive-safaris/index.html'),
+        safaris5day: path.resolve(__dirname, 'kruger-national-park/5-day-safari/index.html'),
+        safaris6day: path.resolve(__dirname, 'kruger-national-park/6-day-safari/index.html'),
+        safaris7day: path.resolve(__dirname, 'kruger-national-park/7-day-safari/index.html'),
         about: path.resolve(__dirname, 'about/index.html'),
         blog: path.resolve(__dirname, 'blog/index.html'),
-        differencecbetweenwhiteandblackrhino: path.resolve(__dirname, 'blog/difference-between-white-and-black-rhino/index.html'),
-        whattobringonasafari: path.resolve(__dirname, 'blog/what-to-bring-on-a-safari/index.html'),
-        whattowearonasafari: path.resolve(__dirname, 'blog/what-to-wear-on-a-safari/index.html'),
         contact: path.resolve(__dirname, 'contact/index.html'),
-        krugernationalpark: path.resolve(__dirname, 'kruger-national-park/index.html'),
-        
-        // General Info
-        airportsguide: path.resolve(__dirname, 'kruger-national-park/airports-guide/index.html'),
-        besttimetovisitkruger: path.resolve(__dirname, 'kruger-national-park/best-time-to-visit/index.html'),
-        krugernationalparkbig5: path.resolve(__dirname, 'kruger-national-park/big-5/index.html'),
-        malaria: path.resolve(__dirname, 'kruger-national-park/malaria-guide/index.html'),
-
-        // Rest Camps
-        restCamps: path.resolve(__dirname, 'kruger-national-park/rest-camps/index.html'),
-
-        worstTime: path.resolve(__dirname, 'kruger-national-park/worst-time-to-visit/index.html'),
-        pundamariarestcamp: path.resolve(__dirname, 'kruger-national-park/rest-camps/punda-maria/index.html'),
-
-        // Wildlife Hub
-        wildlife: path.resolve(__dirname, 'kruger-national-park/wildlife/index.html'),
-        mammals: path.resolve(__dirname, 'kruger-national-park/mammals/index.html'),
-
-         // Mammal Species
-        africanBuffalo: path.resolve(__dirname, 'kruger-national-park/african-buffalo/index.html'),
-        africanBushElephant: path.resolve(__dirname, 'kruger-national-park/african-bush-elephant/index.html'),
-        africanLeopard: path.resolve(__dirname, 'kruger-national-park/african-leopard/index.html'),
-        blackRhinoceros: path.resolve(__dirname, 'kruger-national-park/black-rhinoceros/index.html'),
-        lion: path.resolve(__dirname, 'kruger-national-park/lion/index.html'),
-        whiteRhinoceros: path.resolve(__dirname, 'kruger-national-park/white-rhinoceros/index.html'),
-
-        // Safari Pages
-        safariPackages: path.resolve(__dirname, 'kruger-national-park/safari-packages/index.html'),
-        birdingsafaris: path.resolve(__dirname, 'kruger-national-park/birding-safari/index.html'),
-        photographicSafari: path.resolve(__dirname, 'kruger-national-park/photographic-safari/index.html'),
-        privateSafaris: path.resolve(__dirname, 'kruger-national-park/private-safaris/index.html'),
-        safariAllInclusivePackages: path.resolve(__dirname, 'kruger-national-park/all-inclusive-safaris/index.html'),
-        Safari3Day: path.resolve(__dirname, 'kruger-national-park/3-day-safari/index.html'),
-        Safari5Day: path.resolve(__dirname, 'kruger-national-park/5-day-safari/index.html'),
-        Safari6Day: path.resolve(__dirname, 'kruger-national-park/6-day-safari/index.html'),
-        Safari7Day: path.resolve(__dirname, 'kruger-national-park/7-day-safari/index.html'),
-        safariOnFilm: path.resolve(__dirname, 'kruger-national-park/safari-on-film/index.html'),
-
-        // Weather Hub
-        krugernationalparkweather: path.resolve(__dirname, 'kruger-national-park/weather/index.html'),
-
 
       }
     }
