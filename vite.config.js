ec8e8c24146b580ec2b6945b0e73a76c4a95a2ef
Eugene Safaris <monkeygenesafaris@gmail.com>
2026-07-03 05:31:52 +0100

    complete rewrite

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,56 @@
-import { defineConfig } from 'vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-  base: '/',
+  base: "/",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     rollupOptions: {
       input: {
-
         // Core
-        main: path.resolve(__dirname, 'index.html'),
-        krugernationalpark: path.resolve(__dirname, 'kruger-national-park/index.html'),
-        besttimetovisit: path.resolve(__dirname, 'kruger-national-park/best-time-to-visit/index.html'),
-        big5: path.resolve(__dirname, 'kruger-national-park/wildlife/big-5/index.html'),
-        blackrhinoceros: path.resolve(__dirname, 'kruger-national-park/wildlife/black-rhinoceros/index.html'),
-        buffalo: path.resolve(__dirname, 'kruger-national-park/wildlife/buffalo/index.html'),
-        elephant: path.resolve(__dirname, 'kruger-national-park/wildlife/elephant/index.html'),
-        leopard: path.resolve(__dirname, 'kruger-national-park/wildlife/leopard/index.html'),
-        lion: path.resolve(__dirname, 'kruger-national-park/wildlife/lion/index.html'),
-        whiterhinoceros: path.resolve(__dirname, 'kruger-national-park/wildlife/white-rhinoceros/index.html'),  
-        krugerwildlife: path.resolve(__dirname, 'kruger-national-park/wildlife/index.html'),    
-        allinclusivesafaris: path.resolve(__dirname, 'kruger-national-park/all-inclusive-safaris/index.html'),
-        safaris5day: path.resolve(__dirname, 'kruger-national-park/5-day-safari/index.html'),
-        safaris6day: path.resolve(__dirname, 'kruger-national-park/6-day-safari/index.html'),
-        safaris7day: path.resolve(__dirname, 'kruger-national-park/7-day-safari/index.html'),
-        about: path.resolve(__dirname, 'about/index.html'),
-        blog: path.resolve(__dirname, 'blog/index.html'),
-        contact: path.resolve(__dirname, 'contact/index.html'),
-
-      }
-    }
-  }
-})
+        main: path.resolve(__dirname, "index.html"),
+        krugernationalpark: path.resolve(
+          __dirname,
+          "kruger-national-park/index.html",
+        ),
+        besttimetovisit: path.resolve(
+          __dirname,
+          "kruger-national-park/best-time-to-visit/index.html",
+        ),
+        big5: path.resolve(__dirname, "wildlife/big-5/index.html"),
+        blackrhinoceros: path.resolve(
+          __dirname,
+          "wildlife/black-rhinoceros/index.html",
+        ),
+        buffalo: path.resolve(__dirname, "wildlife/buffalo/index.html"),
+        elephant: path.resolve(__dirname, "wildlife/elephant/index.html"),
+        leopard: path.resolve(__dirname, "wildlife/leopard/index.html"),
+        lion: path.resolve(__dirname, "wildlife/lion/index.html"),
+        whiterhinoceros: path.resolve(
+          __dirname,
+          "wildlife/white-rhinoceros/index.html",
+        ),
+        krugerwildlife: path.resolve(__dirname, "wildlife/index.html"),
+        allinclusivesafaris: path.resolve(
+          __dirname,
+          "kruger-all-inclusive-safaris/index.html",
+        ),
+        safaris: path.resolve(__dirname, "safaris/index.html"),
+        safaris5day: path.resolve(__dirname, "safaris/5-day-kruger/index.html"),
+        safaris6day: path.resolve(__dirname, "safaris/6-day-kruger/index.html"),
+        safaris7day: path.resolve(__dirname, "safaris/7-day-kruger/index.html"),
+        birdingkruger: path.resolve(
+          __dirname,
+          "safaris/birding-kruger/index.html",
+        ),
+        photographickruger: path.resolve(
+          __dirname,
+          "safaris/photographic-kruger/index.html",
+        ),
+        about: path.resolve(__dirname, "about/index.html"),
+        blog: path.resolve(__dirname, "blog/index.html"),
+        contact: path.resolve(__dirname, "contact/index.html"),
+      },
+    },
+  },
+});
