80f76695b7d42bbdb4471eeed1a6f276095c1be3
Eugene Safaris <monkeygenesafaris@gmail.com>
2026-07-05 16:29:18 +0100

    font fixes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import './styles.css';
-import './nav.js';
-import './tabs.js';
-import './weather.js';
+import "./styles.css";
+import "./nav.js";
 
 // Create dataLayer FIRST
 window.dataLayer = window.dataLayer || [];
-window.gtag = function(){ window.dataLayer.push(arguments); };
+window.gtag = function () {
+  window.dataLayer.push(arguments);
+};
 
 // Load gtag script
 const script = document.createElement("script");
@@ -14,8 +14,8 @@ script.src = "https://www.googletagmanager.com/gtag/js?id=G-F2CBZHQXX1";
 
 // Optional but cleaner: wait for script before config
 script.onload = () => {
-  window.gtag('js', new Date());
-  window.gtag('config', 'G-F2CBZHQXX1', {
+  window.gtag("js", new Date());
+  window.gtag("config", "G-F2CBZHQXX1", {
     page_path: window.location.pathname,
   });
 };
