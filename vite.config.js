4d30f4a810faac701891a4fd71fa6fded014f03b
Eugene Safaris <monkeygenesafaris@gmail.com>
2026-08-04 13:50:58 +0100

    changes and fixes

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
         contact: path.resolve(__dirname, "contact/index.html"),
         about: path.resolve(__dirname, "about/index.html"),
         // Safaris
-        allinclusivesafaris: path.resolve(__dirname, "private-kruger-safaris/index.html"),
+        allinclusivesafaris: path.resolve(__dirname, "kruger-private-safari/index.html"),
         birdingkrugertour: path.resolve(__dirname, "kruger-birding-tour/index.html"),
         photographickruger: path.resolve(__dirname, "kruger-photographic-safari/index.html"),
         // Kruger National Park Guide
